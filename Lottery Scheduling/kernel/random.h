f09c8ed3720d163696d1079d3e1fce418a9c7793
Logan Flecke <loganflecke@MacBook-Air-41.local>
2023-10-12 21:14:58 -0400

    Lottery Scheduling

---
+++ Lottery Scheduling/kernel/random.h
@@ -0,0 +1,5 @@
+#ifndef RANDOM_CPP
+#define RANDOM_CPP
+void rand_init(int);
+int scaled_random(int, int);
+#endif
---
+++ Lottery Scheduling/user/ticketsnum.d
@@ -0,0 +1,2 @@
+user/ticketsnum.o: user/ticketsnum.c kernel/types.h kernel/stat.h \
+ user/user.h kernel/param.h kernel/pstat.h kernel/param.h
